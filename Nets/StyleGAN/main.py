0c535939322ee6e24ecbc082f9d70af2efa8f953
NickiHell <nickihell@gmail.com>
2021-11-28 15:05:31 +1000

    Refactor

--- Nets/StyleGAN/main.py
+++ Nets/StyleGAN/main.py
@@ -106,23 +106,6 @@ image_transforms = transforms.Compose([
 data = ImageFolder(root=f'{BASE_DIR}/Datasets/ScarletChoir', transform=image_transforms)
 dataloader = DataLoader(data, batch_size=opt.batch_size, shuffle=True, num_workers=8)
 
-# os.makedirs("data/mnist", exist_ok=True)
-# dataloader = torch.utils.data.DataLoader(
-#     datasets.FashionMNIST(
-#         "data/mnist",
-#         train=True,
-#         download=True,
-#         transform=transforms.Compose([
-#             transforms.Resize(opt.img_size),
-#             transforms.ToTensor(),
-#             transforms.Normalize([0.5], [0.5])
-#         ]
-#         ),
-#     ),
-#     batch_size=opt.batch_size,
-#     shuffle=True,
-# )
-
 # Optimizers
 optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
 optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
