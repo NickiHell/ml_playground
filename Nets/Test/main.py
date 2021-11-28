5d61c75af4616da84c2a1f08b0d41b55f4ffc43d
NickiHell <nickihell@gmail.com>
2021-11-28 15:08:16 +1000

    Refactor

--- Nets/Test/main.py
+++
@@ -1,6 +0,0 @@
-from asrecognition import ASREngine
-
-asr = ASREngine("ru", model_path="jonatasgrosman/wav2vec2-large-xlsr-53-russian")
-
-audio_paths = ["/path/to/file.mp3", "/path/to/another_file.wav"]
-transcriptions = asr.transcribe(audio_paths)
