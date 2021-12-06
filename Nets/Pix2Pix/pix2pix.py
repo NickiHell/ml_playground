1e82244cc761b1a72c92136673449ceda7ce08da
NickiHell <nickihell@gmail.com>
2021-12-06 13:54:56 +1000

    Pix2Pix

--- Nets/Pix2Pix/pix2pix.py
+++ Nets/Pix2Pix/pix2pix.py
@@ -12,8 +12,8 @@ from datasets import *
 from models import *
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--epoch", type=int, default=59, help="epoch to start training from")
-parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
+parser.add_argument("--epoch", type=int, default=161, help="epoch to start training from")
+parser.add_argument("--n_epochs", type=int, default=500, help="number of epochs of training")
 parser.add_argument("--dataset_name", type=str, default="ScarletChoir", help="name of the dataset")
 parser.add_argument("--batch_size", type=int, default=4, help="size of the batches")
 parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
@@ -25,7 +25,7 @@ parser.add_argument("--img_height", type=int, default=256, help="size of image h
 parser.add_argument("--img_width", type=int, default=256, help="size of image width")
 parser.add_argument("--channels", type=int, default=3, help="number of image channels")
 parser.add_argument(
-    "--sample_interval", type=int, default=1500, help="interval between sampling of images from generators"
+    "--sample_interval", type=int, default=1000, help="interval between sampling of images from generators"
 )
 parser.add_argument("--checkpoint_interval", type=int, default=1, help="interval between model checkpoints")
 opt = parser.parse_args()
